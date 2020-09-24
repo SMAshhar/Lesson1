018229a47caf41c1cf8fc796c452c722fc494c46
Syed Muhammad Ashhar <syed.muhammad.ashhar@gmail.com>
2020-09-24 14:06:44 +0500

    PRacticing again

---
+++ practice_2.py
@@ -0,0 +1,76 @@
+ #sample = ["Nice", "Me love it too", "Zoom", "Awesome", "Ah... Always love a classic"]
+# b = len(sample)
+# cars = []
+# x = True
+# import random
+# while x == True:
+#     y = input("What car do you like? \n : ")
+#     if y != "q":
+#         a = random.randrange(1,b)
+#         cars.append(y)
+#         print(sample[a])
+#     else:
+#         break
+# print("So far, car inputs were : ", cars)
+
+# cars.insert(1, "nice")
+
+# # cars.remove("Toyota")
+# del cars[1]
+# print(cars)
+
+# print(sorted(sample, reverse = True))
+# print(sample)
+# print(f"{len(sample)} is the lenght of sample sentences")
+# print(sample[6])
+
+# pizza = ["Pepperoni", "Cheese", "Tikka", "Afghani"]
+
+# for a in pizza:
+#     print(f"I love {a}")
+# print()
+# x = []
+# for a in range(11):
+#     print(a ** 3)
+#     x.append(a)
+# print(x[:3])
+# print(x[4:7])
+# print(x[-3:])
+
+# foodlist = ('pizza', 'falafel', 'carrot cake', 'cannoli', 'ice cream')
+
+
+# foodlist = ("changedfood1", "changedfood2", "changedfood3")
+# for a in foodlist:
+# #     print(a)
+
+# car = 'subaru'
+# print("Is car == 'subaru'? I predict True.")
+# print(car == 'subaru')
+# print("\nIs car == 'audi'? I predict False.")
+# print(car == 'audi')
+
+# if "Nice" in sample:
+#     print(True)
+
+# print(list(x**2 for x in range(0,11)))
+
+# import random
+# x = ["green", "red", "yello"]
+# y = random.randint(1,3)
+# if y == 1:
+#     z = x[0]
+# elif y == 2:
+#     z = x[1]
+# elif y == 3:
+#     z = x[2]
+# print(z)
+
+# user_names = ["Eric", "Admin", "Ali"]
+
+# x = input("Enter your username")
+
+# if x == "Admin":
+#     print(f"Welcome {x}, would you like to see the login report?")
+# elif x in user_names:
+#     print(f"Welcome back {x}")
